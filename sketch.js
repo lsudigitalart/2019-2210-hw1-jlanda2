5ddaa7308bf74680d90331449c41302aa72264b4
Serena Landaiche <jlanda2@lsu.edu>
2019-08-29 22:11:04 -0500

    refined assignment 1

---
+++ sketch.js
@@ -0,0 +1,98 @@
+function setup(){
+    createCanvas(1500, 700);
+    background(0,0,0);
+    strokeWeight(30);
+
+    ellipse(0,0,1400,700);
+    strokeWeight(30);
+    ellipse(0,700,1400,700);
+    strokeWeight(30);
+    ellipse(1100,350,1400,2000);
+    strokeWeight(20);
+    ellipse(800,350,800,685);
+    strokeWeight(15);
+    ellipse(800,350,800,545);
+    strokeWeight(10);
+    ellipse(800,350,800,405);
+    strokeWeight(10);
+    ellipse(800,350,800,265);
+    strokeWeight(5);
+    ellipse(800,350,800,125);
+    fill(0,0,0);
+    strokeWeight(2.5);
+    ellipse(800,350,800,20);
+    fill(255,255,255);
+    strokeWeight(20);
+
+    fill(255,255,255);
+    strokeWeight(20);
+    ellipse(1100,0,800,700);
+    fill(0,0,0);
+    strokeWeight(20);
+    ellipse(1100,0,600,500);
+    fill(255.255,255);
+    strokeWeight(20);
+    ellipse(1100,0,400,300);
+    fill(0,0,0);
+    strokeWeight(20);
+    ellipse(1100,0,200,100);
+    fill(255,255,255);
+    strokeWeight(20);
+    ellipse(1100,700,800,700);
+    fill(0,0,0);
+    strokeWeight(20);
+    ellipse(1100,700,600,500);
+    fill(255,255,255);
+    strokeWeight(20);
+    ellipse(1100,700,400,300);
+    fill(0,0,0);
+    strokeWeight(20);
+    ellipse(1100,700,200,100);
+    fill(255,255,255);
+        
+    strokeWeight(20);
+    ellipse(1500,350,800,700);
+    strokeWeight(15);
+    ellipse(1500,350,735,635);
+    strokeWeight(10);
+    ellipse(1500,350,675,575);
+    strokeWeight(10);
+    ellipse(1900,350,1400,2000);
+    fill(0,0,0);
+    ellipse(1250,350,100,100);
+    fill(255,255,255);
+    fill(0,0,0);
+    ellipse(1345,350,75,75);
+    fill(255,255,255);
+    fill(0,0,0);
+    ellipse(1415,350,50,50);
+    fill(255,255,255);
+    fill(0,0,0);
+    ellipse(1465,350,35,35);
+    fill(255,255,255);
+    fill(0,0,0);
+    ellipse(1500,350,25,25);
+    fill(255,255,255);
+    strokeWeight(30);
+    ellipse(400 , 350, 200, 200);
+
+    fill(255,255,255);
+    strokeWeight(50);
+    ellipse(0,700,600,500);
+    fill(0,0,0);
+    ellipse(0,700,300,300);
+    fill(255,255,255);
+    strokeWeight(50);
+    ellipse(0,0,600,500);
+    fill(0,0,0);
+    ellipse(0,0,300,300);
+    fill(0,0,0);
+    ellipse(1500,0,300,300);
+    fill(0,0,0);
+    ellipse(1500,700,300,300);
+
+}
+
+function draw(){
+
+}
